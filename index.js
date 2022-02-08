bb8843e2b814e22353c1481befd32c09305ccc15
Laura Lu <lulaura011205@gmail.com>
2022-02-07 23:09:14 -0800

    minor syntax improvement

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ client.on("messageCreate", (message) => {
     // if it is a valid command, get userID
     let userID = message.content.split(" ")[1];
     // if userID not specified
-    if (userID == null) {
+    if (userID === null) {
         message.author.send("Enter an user ID after the command in this format: !troll [userID]");
         return;
     }
     // find the user to troll using given userID
     let target = message.guild.members.cache.get(userID);
     // if userID is invalid
-    if (target == null) {
+    if (target === null) {
         message.author.send("Enter a VALID user ID after the command in this format: !troll [userID]");
         return;
     }
     // userID is valid, set the targetID to userID
     targetID = userID;
     // join user's channel if the user is in a voice channel when the command is sent
-    if (target.voice.channel != null) {
+    if (target.voice.channel !== null) {
         joinChannel(target.voice.channel);
     }
 });
@@ -35,9 +35,9 @@ client.on("messageCreate", (message) => {
 // handle voice channel state changes
 client.on("voiceStateUpdate", (oldState, newState) => {
     // if the state change involves the target
-    if (newState.id == targetID) {
+    if (newState.id === targetID) {
         // if the target left a voice channel, leave with them
-        if (newState.channel == null) {
+        if (newState.channel === null) {
             const connection = voiceDiscord.getVoiceConnection(oldState.channel.guild.id);
             connection.destroy();
         }
